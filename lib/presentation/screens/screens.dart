3a430df92f5163cb635509678537041b589ee869
sys <sisupeaaivaras@gmail.com>
2024-04-25 10:38:23 -0400

    screen creadas y barril actualizao

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,10 @@
 // Exportacio de la rutas
+export 'package:widgtes_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widgtes_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgtes_app/presentation/screens/buttons/home_screen.dart';
 export 'package:widgtes_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widgtes_app/presentation/screens/home/home_screen.dart';
+export 'package:widgtes_app/presentation/screens/infinite_scroll/inifinite_scroll_screen.dart';
+export 'package:widgtes_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widgtes_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgtes_app/presentation/screens/ui_controls/ui_controls_screen.dart';
